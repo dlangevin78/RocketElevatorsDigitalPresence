2e8fc5df61c246b2f8af03df78e93bdc41d15197
Daniel Langevin <dlangevin78@gmail.com>
2020-06-08 22:22:54 -0400

    cleanup1

--- Smarty/HTML_BS4/assets/js/quote.js
+++ Smarty/HTML_BS4/assets/js/quote.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var choiceSelect = document.getElementsByClassName('building_type').value;
     console.log(choiceSelect);
       if (choiceSelect == 'Residential-choice') {
-      $('#Residential').show();
+      $('#Residential').innerText.show();
     } else if (choiceSelect == 'Commercial-choice') {
       $('#Commercial').show();
     }else if (choiceSelect == 'Corporative-choice') {
@@ -15,22 +15,29 @@ $(document).ready(function () {
          $('#Hybrid').show();
     }
   });
-//shows on chrome console as a HTML collection (not an array).
+//shows on chrome console as a HTML collection (not an array). Is it my problem? change it into an array??
 
   //Quote calculations to put in the button submit for each building-type
   // Make a radio button inside a popup = (quote?).modal and (inside the html?).modal-content DUNNO
+  // saw the way of making one on #questions of slack...copy paste the method? Not with friday's warning...
 
-  //Residential
+
+  //Residential   NEED TO COME BACK LATER YOU LOOSING TIME!!! rapport avec (n; n<length,; n++) pour +sieurs shaft or jQuery *.each() pr faire un loop
 
   var mean = $('aptnum')/$('residential_floor_num');
-   
-
-  if ('mean' > 20) {};//2 times de number of shaft
-  if ('mean' <= 20){}; // single shaft
-
+  var total1 = 0 ;          //will contain the result of '20 or less' so 1 shaft
+  var total2 = 0 ;          //will contain the result of the 'if more than 20' so 2 shaft
+  var n = 0;           //what if we have 100 floors...I can't make a variable forever! NOT WORKING THE WAY I DO IT
+//put function involving the value of var mean "('*')function('mean'){} " and put the following in fonction -->
+  if ('mean' > 20) {
+        println(total1);        // I know its not println but some jQuery fnct still have to figure this out
+  };//2 times de number of shaft
+  if ('mean' <= 20){            // single shaft
+        println(total1)*2
+    };
   //Commercial
 
-  var commValue = $('commercial_shaft_num');
+  var commValue = $('commercial_shaft_num');        //number the client inputed is the number of shaft proposed
 
   //Corporative
 
@@ -39,22 +46,26 @@ $(document).ready(function () {
   var corpColumn = ($('corporative_floor_num'+ 'corporative_basement_num')/20);
   var corpShaftInColumn = corpshaftNum/corpColumn;
   var CorpTotal = corpShaftInColumn * corpColumn;
+  $('CorpTotal').show();                               //same comment as hybrid so are residential total and commercial value
   
   //Hybrid
  
   var hybrTotalOcc = $('hybrid_occupancy_num')*$('hybrid_floor_num'+'hybrid_basement_num');
-  var hybrshaftNum = hybrcorpTotalOcc/1000;
+  var hybrshaftNum = hybrTotalOcc/1000;
   var hybrColumn = ($('hybrid_floor_num'+ 'hybrid_basement_num')/20);
   var hybrShaftInColumn = hybrShaftNum/hybrColumn;
   var hybrTotal = hybrShaftInColumn * hybrColumn;
+  $('hybrTotal').show();                            //but in the popup1 with the radio button choice underneath once the radio button is done
 
 
-  //get a popup page with the radio button when the fields are completed and the button submit is clicked (standard/premium/excelliu
+  //get a popup2 page with the radio button when the fields are completed and the radio button submit is clicked (standard/premium/excelliu
+  
   //if ('standard-button') is pressed:
   var residStandard = ('dunno') ;
-  var commStandard = ('commValue' * 7565);
+  var commStandard = ('commValue' * 7565)
   var corpoStandard = ('corpTotal' * 7565);
   var hybridStandard = ('hybrTotal' * 7565);
+  //var pctStandard to create ('var *Standard * 0.1)+ "value of the var *Standard"
 
   //if ('premium') is pressed:
 
@@ -62,6 +73,7 @@ $(document).ready(function () {
   var commPremium = ('commValue' * 12345);
   var corpoPremium = ('corpTotal'* 12345);
   var hybridPremium = ('hybrid' * 12345);
+//var pctPremium to create ('var *Premium * 0.1)+ "value of the var *Premium"
 
   //if ('excelium') is pressed:
 
@@ -70,7 +82,10 @@ $(document).ready(function () {
   var corpoExcel = ('corpTotal' * 15400);
   var hybridExcel = ('hybridTotal' * 15400);
 
-  //
+  
+  //button SEE QUOTE to ADD at the end of the popup2
+
+  //final popup that shows in read only the final quote
   
 
   
